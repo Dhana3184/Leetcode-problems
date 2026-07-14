03121d20a235f42c78ee52b6ce22777ed58e07eb
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-14 14:15:34 +0530

    Create 227-BasicCalculatorIi

---
+++ 227-BasicCalculatorIi/227-BasicCalculatorIi.java
@@ -0,0 +1,32 @@
+// Last updated: 14/07/2026, 14:18:35
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> st=new Stack<>();
+        int num=0;
+        char op='+';
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                num=num*10+(ch-'0');
+            }
+            if(!Character.isDigit(ch) && ch!=' ' || i==s.length()-1){
+                if(op=='+'){
+                    st.push(num);
+                }else if(op=='-'){
+                    st.push(-num);
+                }else if(op=='*'){
+                    st.push(st.pop()*num);
+                }else if(op=='/'){
+                    st.push(st.pop()/num);
+                }
+                op=ch;
+                num=0;
+            }
+        }
+        int res=0;
+        while(!st.isEmpty()){
+            res+=st.pop();
+        }
+        return res;
+    }
+}

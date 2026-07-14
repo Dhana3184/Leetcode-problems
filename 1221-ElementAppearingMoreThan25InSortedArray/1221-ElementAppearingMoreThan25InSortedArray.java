7861a6f4718a998e1a045d7cff247795abcfe580
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-14 14:13:13 +0530

    Create 1221-ElementAppearingMoreThan25InSortedArray

---
+++ 1221-ElementAppearingMoreThan25InSortedArray/1221-ElementAppearingMoreThan25InSortedArray.java
@@ -0,0 +1,14 @@
+// Last updated: 14/07/2026, 14:16:14
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            if(map.get(arr[i])> n/4){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}

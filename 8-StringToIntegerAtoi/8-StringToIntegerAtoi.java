f2e6bb75e70c04ae898c79c0d06055cd580477fc
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-14 14:17:27 +0530

    Create 8-StringToIntegerAtoi

---
+++ 8-StringToIntegerAtoi/8-StringToIntegerAtoi.java
@@ -0,0 +1,26 @@
+// Last updated: 14/07/2026, 14:20:28
+class Solution {
+    public int myAtoi(String s) {
+        int num=0;
+        int sign=1;
+        int i=0;
+        while(i<s.length() && s.charAt(i)==' '){
+            i++;
+        }
+        if(i<s.length() && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+            if(s.charAt(i)=='-'){
+                sign=-1;
+            }
+            i++;
+        }
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
+            if(num>Integer.MAX_VALUE/10 || (num==Integer.MAX_VALUE/10 && digit>7)){
+                return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            num=num*10+digit;
+            i++;
+        }
+        return num*sign;
+    }
+}

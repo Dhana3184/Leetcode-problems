a985f9478be493d51d5c2009b55cf2f394f8e3a0
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-14 14:12:06 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,15 @@
+// Last updated: 14/07/2026, 14:15:07
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int[] freq=new int[26];
+        for(int i=0;i<sentence.length();i++){
+            freq[sentence.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

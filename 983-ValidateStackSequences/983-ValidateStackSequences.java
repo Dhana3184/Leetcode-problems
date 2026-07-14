5bd808bc6254e8f261891cb6d744bfb4575408b4
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-14 14:13:32 +0530

    Create 983-ValidateStackSequences

---
+++ 983-ValidateStackSequences/983-ValidateStackSequences.java
@@ -0,0 +1,18 @@
+// Last updated: 14/07/2026, 14:16:33
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st=new Stack<>();
+        int j=0;
+        for(int i=0;i<pushed.length;i++){
+            st.push(pushed[i]);
+            while(!st.isEmpty() && popped[j]==st.peek()){
+                st.pop();
+                j++;
+            }
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}

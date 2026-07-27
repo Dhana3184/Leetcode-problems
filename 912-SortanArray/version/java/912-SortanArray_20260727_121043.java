e4ffe377640293b0b0b9fbccfdf6e2b7a873e33e
Dhana3184 <dhanakavya2004@gmail.com>
2026-07-27 12:10:49 +0530

    Create 912-SortanArray

---
+++ 912-SortanArray/version/java/912-SortanArray_20260727_121043.java
@@ -0,0 +1,16 @@
+// Last updated: 27/07/2026, 12:10:43
+1class Solution {
+2    public int[] sortArray(int[] nums) {
+3        PriorityQueue<Integer> pq=new PriorityQueue<>();
+4        for(int i=0;i<nums.length;i++){
+5            pq.offer(nums[i]);
+6        }
+7        int[] arr=new int[pq.size()];
+8        for(int i=0;i<arr.length;i++){
+9            arr[i]=pq.poll();
+10        }
+11        return arr;
+12        /*Arrays.sort(nums);
+13        return nums;*/
+14    }
+15}

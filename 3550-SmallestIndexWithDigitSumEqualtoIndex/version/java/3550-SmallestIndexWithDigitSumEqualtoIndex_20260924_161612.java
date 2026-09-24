b8a884034054b0bc5191057a1708951913f9c8ff
Dhana3184 <dhanakavya2004@gmail.com>
2026-09-24 16:15:43 +0530

    Create 3550-SmallestIndexWithDigitSumEqualtoIndex

---
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/version/java/3550-SmallestIndexWithDigitSumEqualtoIndex_20260924_161612.java
@@ -0,0 +1,18 @@
+// Last updated: 24/09/2026, 16:16:12
+1class Solution {
+2    public int smallestIndex(int[] nums) {
+3        for(int i=0;i<nums.length;i++){
+4            int n=nums[i];
+5            int sum=0;
+6            while(n>0){
+7                int dig=n%10;
+8                sum+=dig;
+9                n/=10;
+10            }
+11            if(sum==i){
+12                return i;
+13            }
+14        }
+15        return -1;
+16    }
+17}
